d865d8e231aa1caac420c8ee8aa6b1f65899e27c
IDG Administrator <37504074+smad-git@users.noreply.github.com>
2024-11-01 16:10:32 +0000

    python 3.10

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,14 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "idg3devlambdaLayer": {
+      "Arn": "string"
+    },
+    "patientMigration": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
